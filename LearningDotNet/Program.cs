a73383bb4486d4e43291a42f5018bafdcd1bb18d
Ausra <ausra@kasiulai.lt>
2022-12-15 22:12:38 +0200

    Tasks

--- LearningDotNet/Program.cs
+++ LearningDotNet/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearningArrays
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            {
+                string[] colors = { "Red", "Blue", "Green", "Yellow", "White" };
+                for (int i = 0; i < colors.Length; i++)
+                {
+                    Console.WriteLine(colors[i]);
+                }
+
+                int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 }; ;
+                for (int i = numbers.Length; i-- > 0;)
+
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+        }
+    }
+}
